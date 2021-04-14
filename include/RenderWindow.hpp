2b2ed73ca091dca577a9188a27c65b766c55b182
Kevin Poly <PolyKevin@outlook.fr>
2021-04-14 14:06:43 +0400

    added players movement

--- include/RenderWindow.hpp
+++ include/RenderWindow.hpp
@@ -2,6 +2,7 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_ttf.h>
+#include <ctime>
 
 class RenderWindow {//class pour crée des fenétre et autres
 public:
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,20 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_ttf.h>
+#include <iostream>
 #include "RenderWindow.hpp"
+
 
 bool isRunning (true);
 SDL_Event event;
 
+int player1PosY (250);	
+int player2PosY (250);
+
 int main(int argv, char** args)
 {
-	SDL_Init(SDL_INIT_VIDEO);
-	
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {//initialise sdl et verifie si il fonctionne
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Bug Bug pas reussi a init_video :c : %s", SDL_GetError());
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {//initialise sdl et verifie si il fonctionne
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Bug Bug pas reussi a sdl :c : %s", SDL_GetError());
         return 0;
     }
 
@@ -21,21 +24,50 @@ int main(int argv, char** args)
 	}
 	
 	RenderWindow window("Ponk!", 800, 600);//création de l'objet window qui pourra crée une fenétre
+	Uint64 currentTime;
+	Uint64 lastTime = SDL_GetTicks();
+	double deltaTime;
 
 	while (isRunning)//game loop
 	{
-		while (SDL_PollEvent(&event))//input pour quitter la fenétre
-		{
+		const Uint8 *keystates = SDL_GetKeyboardState(NULL);
+
+		while (SDL_PollEvent(&event)) {//input pour quitter la fenétre
+		
+			
 			switch (event.type)
 			{
 			    case SDL_QUIT:
-				isRunning = false;
-				break;
+					isRunning = false;
+					break;
 			}
+
+			
 		}
-		window.loadTexture("data/gfx/G39.png", 10 , 250, 0);//affiche le joueur1
-		window.loadTexture("data/gfx/G39.png", 775, 250, 1);//affiche le joueur2
+
+		currentTime = SDL_GetTicks();//calculating delta time
+		deltaTime = (double)((currentTime - lastTime) * ( 60 / 1000.0f ));
+		lastTime = currentTime;
+
+		if (keystates[SDL_SCANCODE_W] && player1PosY > 0) {//player1 movement
+				player1PosY -= 10.0 * deltaTime;
+			}
+		if (keystates[SDL_SCANCODE_S] && player1PosY < 535) {
+			player1PosY += 10.0 * deltaTime;
+		}
+
+		if (keystates[SDL_SCANCODE_UP] && player2PosY > 0) {//player2 movement
+				player2PosY -= 10.0 * deltaTime;
+			}
+		if (keystates[SDL_SCANCODE_DOWN] && player2PosY < 535) {
+			player2PosY += 10.0 * deltaTime;
+		}
+
+		window.loadTexture("data/gfx/G39.png", 10 , player1PosY, 0);//affiche le joueur1
+		window.loadTexture("data/gfx/G39.png", 775, player2PosY, 1);//affiche le joueur2
 		window.update();
+
+		
 	}
 
 	//vide la mémoire 
